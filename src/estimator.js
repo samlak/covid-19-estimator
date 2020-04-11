10680373ef5b65cd094c5211b07c589c7e6fb662
samlak <samlak1999@gmail.com>
2020-04-11 13:03:19 +0100

    Eslint guideline followed

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 const fs = require('fs');
 
 const express = require('express');
@@ -10,71 +11,71 @@ const NS_PER_SEC = 1e9;
 const MS_PER_NS = 1e-6;
 
 app.use(bodyParser.json());
- 
-var serializer = new EasyXml({
-    singularize: true,
-    rootElement: 'response',
-    dateFormat: 'ISO',
-    manifest: true
+
+const serializer = new EasyXml({
+  singularize: true,
+  rootElement: 'response',
+  dateFormat: 'ISO',
+  manifest: true
 });
 
 const covid19ImpactEstimator = (data) => {
-  let timeToElapse = data.timeToElapse;
+  let { timeToElapse } = data;
 
-  if(data.periodType === 'weeks'){
-    timeToElapse = data.timeToElapse * 7
-  }else if (data.periodType === 'months'){
-    timeToElapse = data.timeToElapse * 30
+  if (data.periodType === 'weeks') {
+    timeToElapse = data.timeToElapse * 7;
+  } else if (data.periodType === 'months') {
+    timeToElapse = data.timeToElapse * 30;
   }
 
   const infectionsByRequestedTimeCalculation = (currentlyInfected) => {
     const factor = Math.trunc(timeToElapse / 3);
-    return Math.trunc(currentlyInfected * (Math.pow(2, factor)));
-  }
-  
+    return Math.trunc(currentlyInfected * (2 ** factor));
+  };
+
   const impact = {
     currentlyInfected: data.reportedCases * 10,
     get infectionsByRequestedTime() {
       return infectionsByRequestedTimeCalculation(this.currentlyInfected);
     },
     get severeCasesByRequestedTime() {
-      return Math.trunc(this.infectionsByRequestedTime * 0.15)
+      return Math.trunc(this.infectionsByRequestedTime * 0.15);
     },
-    get hospitalBedsByRequestedTime(){
-      return Math.trunc((data.totalHospitalBeds * 0.35) - this.severeCasesByRequestedTime)
+    get hospitalBedsByRequestedTime() {
+      return Math.trunc((data.totalHospitalBeds * 0.35) - this.severeCasesByRequestedTime);
     },
-    get casesForICUByRequestedTime(){
-      return  Math.trunc(this.infectionsByRequestedTime * 0.05)
+    get casesForICUByRequestedTime() {
+      return Math.trunc(this.infectionsByRequestedTime * 0.05);
     },
-    get casesForVentilatorByRequestedTime(){
-      return  Math.trunc(this.infectionsByRequestedTime * 0.02)
+    get casesForVentilatorByRequestedTime() {
+      return Math.trunc(this.infectionsByRequestedTime * 0.02);
     },
-    get dollarsInFlight(){
-      return  Number((this.infectionsByRequestedTime * data.region.avgDailyIncomeInUSD * 
-        data.region.avgDailyIncomePopulation * timeToElapse).toFixed(2))
+    get dollarsInFlight() {
+      return Number((this.infectionsByRequestedTime * data.region.avgDailyIncomeInUSD
+      * data.region.avgDailyIncomePopulation * timeToElapse).toFixed(2));
     }
   };
 
   const severeImpact = {
-    currentlyInfected:  data.reportedCases * 50,
+    currentlyInfected: data.reportedCases * 50,
     get infectionsByRequestedTime() {
       return infectionsByRequestedTimeCalculation(this.currentlyInfected);
     },
     get severeCasesByRequestedTime() {
-      return Math.trunc(this.infectionsByRequestedTime * 0.15)
+      return Math.trunc(this.infectionsByRequestedTime * 0.15);
     },
-    get hospitalBedsByRequestedTime(){
-      return Math.trunc((data.totalHospitalBeds * 0.35) - this.severeCasesByRequestedTime)
+    get hospitalBedsByRequestedTime() {
+      return Math.trunc((data.totalHospitalBeds * 0.35) - this.severeCasesByRequestedTime);
     },
-    get casesForICUByRequestedTime(){
-      return  Math.trunc(this.infectionsByRequestedTime * 0.05)
+    get casesForICUByRequestedTime() {
+      return Math.trunc(this.infectionsByRequestedTime * 0.05);
     },
-    get casesForVentilatorByRequestedTime(){
-      return  Math.trunc(this.infectionsByRequestedTime * 0.02)
+    get casesForVentilatorByRequestedTime() {
+      return Math.trunc(this.infectionsByRequestedTime * 0.02);
     },
-    get dollarsInFlight(){
-      return  Number((this.infectionsByRequestedTime * data.region.avgDailyIncomeInUSD * 
-      data.region.avgDailyIncomePopulation * timeToElapse).toFixed(2))
+    get dollarsInFlight() {
+      return Number((this.infectionsByRequestedTime * data.region.avgDailyIncomeInUSD
+      * data.region.avgDailyIncomePopulation * timeToElapse).toFixed(2));
     }
   };
 
@@ -88,7 +89,7 @@ const covid19ImpactEstimator = (data) => {
         hospitalBedsByRequestedTime: impact.hospitalBedsByRequestedTime,
         casesForICUByRequestedTime: impact.casesForICUByRequestedTime,
         casesForVentilatorByRequestedTime: impact.casesForVentilatorByRequestedTime,
-        dollarsInFlight: impact.dollarsInFlight,
+        dollarsInFlight: impact.dollarsInFlight
       },
       severeImpact: {
         currentlyInfected: severeImpact.currentlyInfected,
@@ -97,36 +98,36 @@ const covid19ImpactEstimator = (data) => {
         hospitalBedsByRequestedTime: severeImpact.hospitalBedsByRequestedTime,
         casesForICUByRequestedTime: severeImpact.casesForICUByRequestedTime,
         casesForVentilatorByRequestedTime: severeImpact.casesForVentilatorByRequestedTime,
-        dollarsInFlight: severeImpact.dollarsInFlight,
+        dollarsInFlight: severeImpact.dollarsInFlight
       }
     }
+  };
+};
+
+const fetchLogs = () => {
+  try {
+    const noteString = fs.readFileSync('logs.txt').toString();
+    return noteString;
+  } catch (e) {
+    return '';
   }
 };
 
-var fetchLogs = () => {
-  try {
-      var noteString = fs.readFileSync('logs.txt').toString();
-      return noteString;
-  } catch (e) {
-      return '';
-  }
-}
-
-var addLogs = (method, path, status, time) => {
+const addLogs = (method, path, status, time) => {
   let logs = fetchLogs();
-  let log = `${method}\t\t${path}\t\t${status}\t\t${time}\n`
+  const log = `${method}\t\t${path}\t\t${status}\t\t${time}\n`;
   logs += log;
   fs.writeFileSync('logs.txt', logs);
-}
+};
 
 // console.log(fetchLogs());
 
 app.post('/api/v1/on-covid-19', async (req, res) => {
   const time = process.hrtime();
-  try{
+  try {
     const estimator = covid19ImpactEstimator(req.body);
     res.status(200).send(estimator);
-  } catch(e){
+  } catch (e) {
     res.status(400).send({
       error: 'There was an error with your input. Make sure you insert the parameters correctly'
     });
@@ -138,11 +139,11 @@ app.post('/api/v1/on-covid-19', async (req, res) => {
 
 app.post('/api/v1/on-covid-19/xml', async (req, res) => {
   const time = process.hrtime();
-  try{
+  try {
     const estimator = serializer.render(covid19ImpactEstimator(req.body));
     res.header('Content-Type', 'text/xml');
     res.status(200).send(estimator);
-  } catch(e){
+  } catch (e) {
     res.status(400).send(
       serializer.render({
         error: 'There was an error with your input. Make sure you insert the parameters correctly'
@@ -159,7 +160,8 @@ app.get('/api/v1/on-covid-19/logs', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Listening to port ${port}`)
-}); 
+  // eslint-disable-next-line no-console
+  console.log(`Listening to port ${port}`);
+});
 
-module.exports = {covid19ImpactEstimator};
+module.exports = { covid19ImpactEstimator };
