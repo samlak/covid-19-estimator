82cef3001ce99f582ec9f7c3ac3b82a9240b4dcc
samlak <samlak1999@gmail.com>
2020-04-13 07:36:04 +0100

    Trying!

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const EasyXml = require('easyxml');
 
-const { covid19ImpactEstimator } = require('./estimator');
+const covid19ImpactEstimator = require('./estimator');
 
 const app = express();
 const port = process.env.PORT || 3000;
